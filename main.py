04e26d6e43ae5a4f08e3f8f655a3ab204a9974b3
thisisaldi <m.naufaldi.aldi@gmail.com>
2024-11-16 19:58:38 +0700

    fix: refactor for better development

--- main.py
+++ main.py
@@ -1,47 +1,24 @@
 import discord
 import os
+import asyncio
+
 from dotenv import load_dotenv
+from cogs.autorole import Autorole
 
 load_dotenv()
 
-token = os.getenv("DISCORD_TOKEN")
+async def setup(bot, cog):
+    await bot.add_cog(cog(bot))
 
-intents = discord.Intents.default()
-intents.message_content = True
+if __name__ == '__main__':
+    token = os.getenv("DISCORD_TOKEN")
 
-client = discord.Client(intents=intents)
+    intents = discord.Intents.default()
+    intents.message_content = True
 
-format = [
-    'nama',
-    'nama panggilan',
-    'angkatan',
-    'hobi',
-    'minat'
-]
+    bot = discord.ext.commands.Bot(command_prefix='$', intents=intents)
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+    # Setup cogs here!
+    asyncio.run(setup(bot, Autorole))
     
-    if message.channel.id != 1237280039771439226:
-        return
-    
-    if all(item in message.content.lower() for item in format):
-        message_content = [msg.split(':') for msg in message.content.split('\n')]
-        for index, content in enumerate(message_content):
-            if any('panggilan' in cont.lower() for cont in content):
-                nickname = message_content[index][1]
-                break
-            
-        print(message.content)
-        role = discord.utils.get(message.guild.roles, name="Informatikans")
-        await message.add_reaction('👋')
-        await message.channel.send(f'{message.author.mention} Salam kenal, {nickname.strip().title()}! Selamat bergabung di Informatikans!')
-        await message.author.add_roles(role)
-
-client.run(token)
+    bot.run(token)
